07fb6222a239d02e9acd48477bb9800d08b14026
et <t.evtiukhin@yandex.ru>
2018-01-05 21:25:35 +0300

    first update

---
+++ source/app.js
@@ -0,0 +1,5 @@
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    Application = require('./components/Application.react');
+
+ReactDOM.render(<Application/>, document.getElementById('react-application'));
