a3bc2da924086e29ceae70be40cac435d5bfac95
et <t.evtiukhin@yandex.ru>
2018-01-06 17:12:23 +0300

    Header.react.js

--- source/components/StreamTweet.react.js
+++ source/components/StreamTweet.react.js
@@ -61,9 +61,22 @@ var React = requre('react'),
             window.snapterest.numberOfReceivedTweets++;
         },
 
+        shouldComponentUpdate: function (nextProps, nextState) {
+            console.log('[Snapterest] StremTweet: 5. Running shouldComponentUpdate');
+            return (nextProps.tweet.text.length > 1);
+        },
+
+        componentWillUpdate: function (nextProps, nextState) {
+            console.log('[Snapterest] StreamTweet: 6. Running componentWillUpdate()');
+        },
+
+        componentDidUpdate: function (prevProps, prevState) {
+            console.log('[Snapterest] StreamTweet: 7. Running componentDidUpdate()');
+            window.snapterest.numberOfDisplayedTweets++;
+        },
+
         componentWillUnmount: function () {
             console.log('[Snapterest] StreamTweet: 8, Running componentWillUnmount()');
-
             delete window.snapterest;
         },
 
